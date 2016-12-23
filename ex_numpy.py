95bda8dae39297d6ad629f0975192aa224818377
Pimin Konstantin Kefaloukos <kostas@audienceproject.com>
2016-12-23 23:47:49 +0100

    Bolling bands

--- ex_numpy.py
+++ ex_numpy.py
@@ -1,18 +1,39 @@
 import numpy as np
 
-# Generate random arrays
+#
 ## https://docs.scipy.org/doc/numpy/reference/routines.random.html
 
-# Normal distribution
-## 1D array
-a = np.random.normal(100, 10, 3)
-## 2D array
-a = np.random.normal(100, 10, (5, 7))
+# Generate random arrays
+## 1D array - normal distribution
+a = np.random.normal(100, 10, 3) # mean, variance, shape
+## 2D array - uniform distrution
+a = np.random.uniform(0, 10, (5, 7)) # min, max, shape
+## 1D array - uniform distrubtion - other method
+b = np.random.rand(10)
 
 # Slicing
 a[0, 1:3]
 a[1:3, 1:3]
 a[[0,2,4], 2]
+a[0:5:2, :]
+a[:, 6] = range(-10, -5)
 
-# Mean
+# Predicate indexing
+mean_b = b.mean()
+bool_idx = b<mean_b
+b[bool_idx]
+# same as
+b[b<b.mean()]
+
+
+# Assignment
+a[0, 0] = 1
+a[1:3, 1:3] = 2
+a[-1, :] = 3
+
+# Mean + 33 other
 a.mean()
+a.median()
+a.std()
+
+# Rolling statistics
--- ex_pandas.py
+++ ex_pandas.py
@@ -9,28 +9,22 @@ def debug(df, message='DataFrame'):
     print(df.tail())
     print()
 
-# Creates empty dataframe
-start_date = '2015-01-01'
+# Create empty dataframe
+start_date = '2006-01-01'
 end_date = '2017-01-01'
 dates = pd.date_range(start_date, end_date)
 df1 = pd.DataFrame(index=dates)
 df1 = df1.sort_index()
 
-debug(df1, 'empty')
-
-# Read SPY data
+# Join SPY
 dfSPY = pd.read_csv(
     'data/SPY.csv', parse_dates=True, index_col='Date',
     usecols=['Date', 'Adj Close'], na_values=['nan'])
 dfSPY = dfSPY.rename(columns={'Adj Close': 'SPY'})
 dfSPY = dfSPY.sort_index()
-
-debug(dfSPY, 'w. SPY')
-
-# Join SPY
 df1 = df1.join(dfSPY, how='inner')
 
-# Add more symbols
+# Join AAPL, GOOG, GLD
 symbols = ['AAPL', 'GOOG', 'GLD']
 for symbol in symbols:
     df_temp = pd.read_csv(
@@ -40,20 +34,32 @@ for symbol in symbols:
 
     df1 = df1.join(df_temp)
 
-debug(df1, 'w. all symbols')
+# Slice
+df_slice = df1.ix['2015-05-01':'2017-05-31', ['AAPL', 'GOOG']]
 
-# Normalize by first row
-df1_norm = df1 / df1.ix[0,:]
+# Normalize (by first row)
+df_normalized = df1 / df1.ix[0,:]
 
-debug(df1_norm, 'normalized')
+# Global statistics, i.e mean + 33 other
+print('-------')
+print('Global statistics')
+print(df1.mean())
+print(df1.median())
+print(df1.std())
+print()
 
-# Slicing
-df1_slice = df1.ix['2010-01-01':'2012-01-01', ['AAPL', 'GOOG']]
-
-debug(df1_slice, 'sliced')
-
+# Bollinger bands
+df_slice = df1.ix['2015-01-01':'2015-12-31', 'SPY']
+rm_SPY = df_slice.rolling(center=False,window=20).mean()
+rstd_SPY = df_slice.rolling(center=False,window=20).std()
+lower_band, upper_band = rm_SPY + rstd_SPY * 2, rm_SPY - rstd_SPY * 2
 
 # Plot
-#df1.plot()
-#plt.title('Closing prices (adjusted)')
-#plt.show()
+ax = df_slice.plot(title='SPY rolling mean', label='SPY')
+rm_SPY.plot(label='Rolling mean', ax=ax)
+lower_band.plot(label='Lower band', ax=ax)
+upper_band.plot(label='Upper band', ax=ax)
+ax.set_xlabel('Date')
+ax.set_ylabel('Price')
+ax.legend('upper left')
+plt.show()
