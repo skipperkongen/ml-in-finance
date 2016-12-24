f55f4c0e5527dc23f7d6a8ff0a2993ceb3e5a4bf
Pimin Konstantin Kefaloukos <kostas@audienceproject.com>
2016-12-24 13:23:42 +0100

    Finished scatter plots

--- ex_dailyreturns_scatter.py
+++ ex_dailyreturns_scatter.py
@@ -1,11 +1,30 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 from util import *
 
-df = get_data(['SPY', 'GLD', 'AAPL'], '2009-01-10', '2012-12-31')
+df = get_data(['SPY', 'XOM', 'GLD'], '2009-01-10', '2012-12-31')
 dr = get_daily_returns(df)
 
-# Plot histogram
-dr.plot(kind='scatter', x='SPY', y='AAPL')
+# Compute stats
+beta_XOM, alpha_XOM = np.polyfit(dr['SPY'], dr['XOM'], 1)
+beta_GLD, alpha_GLD = np.polyfit(dr['SPY'], dr['GLD'], 1)
+correlation = dr.corr(method='pearson')
+
+# Print stats
+print('beta XOM =', beta_XOM)
+print('alpha XOM =', alpha_XOM)
+print('beta GLD =', beta_GLD)
+print('alpha GLD =', alpha_GLD)
+print('Correlation:')
+print(correlation)
+
+# Scatter plot data
+dr.plot(kind='scatter', x='SPY', y='XOM')
+plt.plot(dr['SPY'], beta_XOM*dr['SPY'] + alpha_XOM, '-', color='r')
+plt.show()
+
+dr.plot(kind='scatter', x='SPY', y='GLD')
+plt.plot(dr['SPY'], beta_GLD*dr['SPY'] + alpha_GLD, '-', color='r')
 plt.show()
